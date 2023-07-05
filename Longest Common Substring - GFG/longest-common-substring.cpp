ca56313104068a9f4474da4714a19dea24221f00
Abhishek chauhan <chauhanabhishek2403@gmail.com>
2023-07-05 13:08:18 +0530

    Added solution - LeetHub

--- Longest Common Substring - GFG/longest-common-substring.cpp
+++ Longest Common Substring - GFG/longest-common-substring.cpp
@@ -6,27 +6,28 @@ using namespace std;
 class Solution{
     public:
     
-    int longestCommonSubstr (string s1, string s2, int n, int m)
+    int longestCommonSubstr (string S1, string S2, int n, int m)
     {
-        int l1 = s1.size();
-        int l2 = s2.size();
-        vector<vector<int>> dp(l1+1,vector<int>( l2+1,-1));
-        for(int i =0 ; i<= l1; i++) dp[i][0] =0;
-        for(int j =0 ; j<= l2; j++) dp[0][j] =0;
-        int ans =0;
-        for(int i =1 ; i<= l1; i++) {
-            for(int j =1 ; j<= l2; j++){
-                if(s1[i-1]==s2[j-1]){
-               dp[i][j] =1+ dp[i-1][j-1];
-               ans = max(ans,dp[i][j]);
-            }
-            else{
-            dp[i][j] = 0;
+       int l1 = S1.length();
+        int l2 = S2.length();
+        vector<vector<int>> dp(l1+1,vector<int>(l2+1,0));
+        for(int i =0;i<=l1;i++){
+            dp[i][0] =0;
         }
-            }
+        for(int j =0;j<=l2;j++){
+            dp[0][j] =0;
         }
-        return ans;
+        int maxLength =0;
+        
+        for(int i=1;i<=l1;i++){
+            for(int j=1;j<=l2;j++){
+                if(S1[i-1] == S2[j-1]) {dp[i][j]= 1 +dp[i-1][j-1];}
+                 maxLength=max(maxLength,dp[i][j]);
+        }   
+        }
+        return maxLength;
     }
+    
 };
 
 //{ Driver Code Starts.
